e45c11dc91dcf96d5be1cac17752cca78efe1013
David Lemayian <david@codeforafrica.org>
2014-04-13 16:56:45 +0300

    Until we get it working well.

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php
+
+// Change default theme colours
+
+//function cfa_dbootcamp_customize_register( $wp_customize ) {
+//	$wp_customize->get_setting( 'et_divi[font_color]' )->default = '#404042';
+//	$wp_customize->get_setting( 'et_divi[link_color]' )->default = '#0873bb';
+//	$wp_customize->get_setting( 'et_divi[accent_color]' )->default = '#0873bb';
+//	$wp_customize->get_setting( 'et_divi[footer_bg]' )->default = '#404042';
+//	$wp_customize->get_setting( 'et_divi[menu_link]' )->default = '#404042';
+//	$wp_customize->get_setting( 'et_divi[menu_link_active]' )->default = '#0873bb';
+//}
+//add_action( 'customize_register', 'cfa_dbootcamp_customize_register' );
+
+
+
+
